43737cc2a38632ace54fc1ea09609267f9237513
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-06-26 10:54:16 +0200

    first commit

---
+++ js/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', function(){
+
+ ReactDOM.render(<h1>Hello</h1>,document.querySelector("#app"));
+
+
+
+
+
+
+
+
+
+
+
+}); 
